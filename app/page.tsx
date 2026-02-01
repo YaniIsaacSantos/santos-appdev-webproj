a0ae8da50976641aa126708a9d37b14bab3de5f8
YaniIsaacSantos <156795259+YaniIsaacSantos@users.noreply.github.com>
2026-02-01 07:09:06 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ app/page.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import Image from "next/image";
+
+export default function Home() {
+  return (
+     <div>
+      <h1>My First Next.js Website!</h1>
+      <p>Welcome to my site deployed on Vercel</p>
+      <style jsx>{`
+        div {
+          padding: 40px;
+          text-align: center;
+          font-family: Arial, sans-serif;
+        }
+        h1 {
+          color: #0070f3;
+        }
+      `}</style>
+    </div>
+  );
+}
